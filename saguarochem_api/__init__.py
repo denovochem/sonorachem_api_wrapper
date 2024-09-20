57f81e8c5f4eacb1ad67184a4bdf8a8148d23aad
Carson Britt <carson.britt@denovochem.com>
2024-09-20 15:28:22 -0700

    Update __init__.py

--- saguarochem_api/__init__.py
+++ saguarochem_api/__init__.py
@@ -1,5 +1,5 @@
-"""saguarochem_api_frontend initialization."""
-__name__ = "SaguaroChem API Wrapper"
+"""sonorachem_api_frontend initialization."""
+__name__ = "SonoraChem API Wrapper"
 __version__ = "0.0.1"
 
-from .core import SaguaroChemAPIWrapper
+from .core import SonoraChemAPIWrapper
