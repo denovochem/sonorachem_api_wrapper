1748d5765f28ade738e1d0dd58ef400d46a9cad7
Carson Britt <carson.s.britt@gmail.com>
2024-09-19 18:46:41 -0700

    Update core.py

--- saguarochem_api/core.py
+++ saguarochem_api/core.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import time
 from typing import Dict, Any, Optional
 
 class SaguaroChemAPIWrapper:
@@ -10,7 +11,7 @@ class SaguaroChemAPIWrapper:
     def __init__(
         self,
         api_key: str,
-        base_url: Optional[str] = None,
+        base_url: Optional[str] = 'https://hwsvflxtqh.execute-api.us-east-2.amazonaws.com/denovochem_api_stage/runpod',
     ):
         """
         SaguaroChemAPIWrapper constructor.
@@ -18,7 +19,7 @@ class SaguaroChemAPIWrapper:
         Args:
             api_key (str): an API key to access the service.
             base_url (str, optional): base url for the service. If not provided it will default to
-                xxxx
+                the De Novo Chem AWS server
 
         Examples:
             Initialize the wrapper by simply providing an API key:
@@ -129,8 +130,10 @@ class SaguaroChemAPIWrapper:
         logger.info("Set API key to {}".format(api_key))
         self._api_key = api_key
         self._headers = self._construct_headers()
+        
+    # def check_api_usage():
 
-    def predict_procedures_retro_template_free(self, smiles, sampling_method='top_k', seq_length=512, beam_size=5, temperature=1.0):
+    def predict_procedures_retro_template_free(self, smiles, sampling_method='greedy', seq_length=256, beam_size=5, temperature=0.3):
       """
       Predicts retrosynthetic procedures for given SMILES strings using a template-free approach.
   
@@ -200,12 +203,218 @@ class SaguaroChemAPIWrapper:
                     }
                 }
 
+      start = time.time()
       output_data = self._send_post_request(self._base_url, self._headers, input_data)
+      returned_data = {'output': output_data['output'], 'status': output_data['status'], 'execution_time': time.time()-start}
 
-      return output_data
+      return returned_data
       
-    # def predict_purification_protocols():
+    def predict_purification_protocols(self, smiles, sampling_method='greedy', seq_length=256, beam_size=5, temperature=0.3):
+      """
+      Predicts purification procedures for given reaction SMILES strings.
+  
+      This function takes a list of reaction SMILES strings and predicts potential 
+      purification procedures. The function allows for different sampling methods and 
+      parameters to control the prediction process.
+  
+      Args:
+          smiles (list of str): A list of SMILES strings representing chemical compounds.
+          sampling_method (str, optional): The method used for sampling predictions. 
+              Must be one of 'top_k', 'greedy', or 'sampling'. Defaults to 'top_k'.
+          seq_length (int, optional): The maximum sequence length for the model input. 
+              Defaults to 512.
+          beam_size (int, optional): The beam size for beam search (if applicable). 
+              Defaults to 5.
+          temperature (float, optional): The temperature parameter for controlling randomness 
+              in sampling. Must be a positive float. Higher values increase randomness. 
+              Defaults to 1.0.
+  
+      Returns:
+          list of list of str: A list of predicted retrosynthetic procedures for each input SMILES.
+              Each procedure is represented as a list of strings describing synthetic steps.
+  
+      Raises:
+          ValueError: If an invalid sampling_method is provided or if temperature is not positive.
+      """
+      # Validate input parameters
+      if not isinstance(smiles, list) or not all(isinstance(s, str) for s in smiles):
+          raise ValueError("The 'smiles' argument must be a list of strings.")
       
-    # def predict_forward_reaction():
+      if sampling_method not in ['top_k', 'greedy', 'sampling']:
+          raise ValueError("Invalid sampling method. Must be 'top_k', 'greedy', or 'sampling'.")
+
+      if not isinstance(seq_length, int):
+          raise ValueError("seq_length must be an integer.")
+      if seq_length <= 0 or seq_length > 512:
+          raise ValueError("seq_length must be greater than 0 and less than or equal to 512.")
+  
+      if not isinstance(beam_size, int):
+          raise ValueError("beam_size must be an integer.")
+      if beam_size <= 0 or beam_size > 16:
+          raise ValueError("beam_size must be greater than 0 and less than or equal to 16.")
+        
+      if beam_size == 1:
+          sampling_method = 'greedy'
       
-    # def predict_procedures_given_reactants_products():
+      if temperature <= 0:
+          raise ValueError("Temperature must be a positive float.")
+
+      input_data = {
+                    "endpoint": "purification_protocols",
+                    "data": {
+                            "inference_strategy": "N/A",
+                            "smiles": smiles,
+                            "kwargs": {
+                                      "sampling_method": sampling_method,
+                                      "seq_length": seq_length,
+                                      "beam_size": beam_size,
+                                      "temperature": temperature
+                                      }
+                            }
+                    }
+
+      start = time.time()
+      output_data = self._send_post_request(self._base_url, self._headers, input_data)
+      returned_data = {'output': output_data['output'], 'status': output_data['status'], 'execution_time': time.time()-start}
+
+      return returned_data
+      
+    def predict_forward_reaction(self, smiles, sampling_method='greedy', seq_length=256, beam_size=5, temperature=0.3):
+      """
+      Predicts products given reactants SMILES using a template-free approach.
+  
+      This function takes a list of SMILES strings and predicts potential products
+      The function allows for different sampling methods and parameters to control the 
+      prediction process.
+  
+      Args:
+          smiles (list of str): A list of SMILES strings representing chemical compounds.
+          sampling_method (str, optional): The method used for sampling predictions. 
+              Must be one of 'top_k', 'greedy', or 'sampling'. Defaults to 'top_k'.
+          seq_length (int, optional): The maximum sequence length for the model input. 
+              Defaults to 512.
+          beam_size (int, optional): The beam size for beam search (if applicable). 
+              Defaults to 5.
+          temperature (float, optional): The temperature parameter for controlling randomness 
+              in sampling. Must be a positive float. Higher values increase randomness. 
+              Defaults to 1.0.
+  
+      Returns:
+          list of list of str: A list of predicted retrosynthetic procedures for each input SMILES.
+              Each procedure is represented as a list of strings describing synthetic steps.
+  
+      Raises:
+          ValueError: If an invalid sampling_method is provided or if temperature is not positive.
+      """
+      # Validate input parameters
+      if not isinstance(smiles, list) or not all(isinstance(s, str) for s in smiles):
+          raise ValueError("The 'smiles' argument must be a list of strings.")
+      
+      if sampling_method not in ['top_k', 'greedy', 'sampling']:
+          raise ValueError("Invalid sampling method. Must be 'top_k', 'greedy', or 'sampling'.")
+
+      if not isinstance(seq_length, int):
+          raise ValueError("seq_length must be an integer.")
+      if seq_length <= 0 or seq_length > 512:
+          raise ValueError("seq_length must be greater than 0 and less than or equal to 512.")
+  
+      if not isinstance(beam_size, int):
+          raise ValueError("beam_size must be an integer.")
+      if beam_size <= 0 or beam_size > 16:
+          raise ValueError("beam_size must be greater than 0 and less than or equal to 16.")
+        
+      if beam_size == 1:
+          sampling_method = 'greedy'
+      
+      if temperature <= 0:
+          raise ValueError("Temperature must be a positive float.")
+
+      input_data = {
+                    "endpoint": "forward_reaction",
+                    "data": {
+                            "inference_strategy": "N/A",
+                            "smiles": smiles,
+                            "kwargs": {
+                                      "sampling_method": sampling_method,
+                                      "seq_length": seq_length,
+                                      "beam_size": beam_size,
+                                      "temperature": temperature
+                                      }
+                            }
+                    }
+
+      start = time.time()
+      output_data = self._send_post_request(self._base_url, self._headers, input_data)
+      returned_data = {'output': output_data['output'], 'status': output_data['status'], 'execution_time': time.time()-start}
+
+      return returned_data
+      
+    def predict_procedures_given_reactants_products(self, smiles, sampling_method='greedy', seq_length=256, beam_size=5, temperature=0.3):
+      """
+      Predicts retrosynthetic procedures for given reactants and products SMILES strings.
+  
+      This function takes a list of reactants and products SMILES strings and predicts potential
+      procedures. The function allows for different sampling methods and parameters to control 
+      the prediction process.
+  
+      Args:
+          smiles (list of str): A list of SMILES strings representing chemical compounds.
+          sampling_method (str, optional): The method used for sampling predictions. 
+              Must be one of 'top_k', 'greedy', or 'sampling'. Defaults to 'top_k'.
+          seq_length (int, optional): The maximum sequence length for the model input. 
+              Defaults to 512.
+          beam_size (int, optional): The beam size for beam search (if applicable). 
+              Defaults to 5.
+          temperature (float, optional): The temperature parameter for controlling randomness 
+              in sampling. Must be a positive float. Higher values increase randomness. 
+              Defaults to 1.0.
+  
+      Returns:
+          list of list of str: A list of predicted retrosynthetic procedures for each input SMILES.
+              Each procedure is represented as a list of strings describing synthetic steps.
+  
+      Raises:
+          ValueError: If an invalid sampling_method is provided or if temperature is not positive.
+      """
+      # Validate input parameters
+      if not isinstance(smiles, list) or not all(isinstance(s, str) for s in smiles):
+          raise ValueError("The 'smiles' argument must be a list of strings.")
+      
+      if sampling_method not in ['top_k', 'greedy', 'sampling']:
+          raise ValueError("Invalid sampling method. Must be 'top_k', 'greedy', or 'sampling'.")
+
+      if not isinstance(seq_length, int):
+          raise ValueError("seq_length must be an integer.")
+      if seq_length <= 0 or seq_length > 512:
+          raise ValueError("seq_length must be greater than 0 and less than or equal to 512.")
+  
+      if not isinstance(beam_size, int):
+          raise ValueError("beam_size must be an integer.")
+      if beam_size <= 0 or beam_size > 16:
+          raise ValueError("beam_size must be greater than 0 and less than or equal to 16.")
+        
+      if beam_size == 1:
+          sampling_method = 'greedy'
+      
+      if temperature <= 0:
+          raise ValueError("Temperature must be a positive float.")
+
+      input_data = {
+                    "endpoint": "procedures_given_reactants_products",
+                    "data": {
+                            "inference_strategy": "N/A",
+                            "smiles": smiles,
+                            "kwargs": {
+                                      "sampling_method": sampling_method,
+                                      "seq_length": seq_length,
+                                      "beam_size": beam_size,
+                                      "temperature": temperature
+                                      }
+                            }
+                    }
+
+      start = time.time()
+      output_data = self._send_post_request(self._base_url, self._headers, input_data)
+      returned_data = {'output': output_data['output'], 'status': output_data['status'], 'execution_time': time.time()-start}
+
+      return returned_data
